68985d623f156620b26602a410f71528a67db01d
Zac Skalko <zskalko@gmail.com>
2025-02-10 23:22:09 -0600

    fmt

--- src/sdl2_display.rs
+++ src/sdl2_display.rs
@@ -1,9 +1,9 @@
+use crate::config::{HEIGHT, WIDTH};
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::RgbColor;
 use sdl2::pixels::Color;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
-use embedded_graphics::pixelcolor::Rgb565;
-use crate::config::{WIDTH, HEIGHT};
-use embedded_graphics::prelude::RgbColor;
 
 /// SDL2 Display wrapper
 pub struct SDL2Display {
@@ -12,7 +12,8 @@ pub struct SDL2Display {
 
 impl SDL2Display {
     pub fn new(video_subsystem: &sdl2::VideoSubsystem) -> Self {
-        let window = video_subsystem.window("Embedded UI", WIDTH as u32, HEIGHT as u32)
+        let window = video_subsystem
+            .window("Embedded UI", WIDTH as u32, HEIGHT as u32)
             .position_centered()
             .build()
             .unwrap();
@@ -27,8 +28,11 @@ impl SDL2Display {
             let y = (i / WIDTH) as i32;
 
             let rgb = embedded_graphics::pixelcolor::Rgb888::from(*pixel);
-            self.canvas.set_draw_color(Color::RGB(rgb.r(), rgb.g(), rgb.b()));
-            self.canvas.draw_point(sdl2::rect::Point::new(x, y)).unwrap();
+            self.canvas
+                .set_draw_color(Color::RGB(rgb.r(), rgb.g(), rgb.b()));
+            self.canvas
+                .draw_point(sdl2::rect::Point::new(x, y))
+                .unwrap();
         }
         self.canvas.present();
     }
