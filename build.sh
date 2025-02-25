ad4fb7a63b967635d76e5d137e7a2df8ad34bb55
Zac Skalko <zskalko@gmail.com>
2025-02-25 00:37:58 -0600

    fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cargo build --release --target thumbv6m-none-eabi
-elf2uf2-rs target/thumbv6m-none-eabi/release/simple_lcd target/thumbv6m-none-eabi/release/simple_lcd.uf2
-cp target/thumbv6m-none-eabi/release/simple_lcd.uf2 /Volumes/RPI-RP2/simple_lcd.uf2
+elf2uf2-rs target/thumbv6m-none-eabi/release/lilbud target/thumbv6m-none-eabi/release/lilbud.uf2
+cp target/thumbv6m-none-eabi/release/lilbud.uf2 /Volumes/RPI-RP2/lilbud.uf2
